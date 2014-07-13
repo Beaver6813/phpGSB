05098d9b6a807962867614ab84c1d6f00c138a6d
Stefan Meinecke <meinecke@greensec.de>
2014-07-13 18:13:56 +0200

    updated to use config and replaced comments.

--- listupdater.php
+++ listupdater.php
@@ -1,17 +1,18 @@
 <?php
-/*
-phpGSB - PHP Google Safe Browsing Implementation
-Released under New BSD License (see LICENSE)
-Copyright (c) 2010-2012, Sam Cleaver (Beaver6813, Beaver6813.com)
-All rights reserved.
-
-UPDATER EXAMPLE
-*/
+/**
+ * phpGSB - PHP Google Safe Browsing Implementation
+ * Released under New BSD License (see LICENSE)
+ * Copyright (c) 2010-2012, Sam Cleaver (Beaver6813, Beaver6813.com)
+ * All rights reserved.
+ *
+ * UPDATER EXAMPLE
+ */
+include("config.php");
 require("phpgsb.class.php");
-$phpgsb = new phpGSB("DATABASE_NAME","DATABASE_USERNAME","DATABASE_PASSWORD");
-//Obtain an API key from: http://code.google.com/apis/safebrowsing/key_signup.html
-$phpgsb->apikey = "API_KEY_HERE";
+$phpgsb = new phpGSB($config['db'], $config['user'], $config['pass'], $config['host']);
+
+// Obtain an API key from: http://code.google.com/apis/safebrowsing/key_signup.html
+$phpgsb->apikey = $config['api_key'];
 $phpgsb->usinglists = array('googpub-phish-shavar','goog-malware-shavar');
 $phpgsb->runUpdate();
 $phpgsb->close();
-?>
